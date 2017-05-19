e756894f9ca1499f411fc3b80f7616a751672ce8
sirajus Salayhin <salayhin@gmail.com>
2017-05-19 16:30:16 +0600

    Created a sample reactive app witht server.R and ui.R

--- census-app/server.R
+++ census-app/server.R
@@ -2,4 +2,12 @@ library(shiny)
 
 shinyServer(function(input, output){
   
+  output$text1 <- renderText({
+    paste("You have selected this", input$var)
+  })
+  
+  output$text2 <- renderText({
+    paste("You have chosen a range that goes from", input$range[1], "to", input$range[2])
+  })
+  
 })
--- census-app/ui.R
+++ census-app/ui.R
@@ -1,47 +1,28 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  
-  titlePanel(h1("My Shiny App")),
-  
+  titlePanel("censusVis"),
   
   sidebarLayout(
     
     sidebarPanel(
-      h1("Installation"),
-      p("Shiny is available on CRAN, so you can install it in the usual way from your R console:"),
-      code('install.packages("shiny")'),
-      br(),
-      br(),
-      br(),
-      br(),
-      selectInput("select", label = h3("Choose a variable to display"), 
-                  choices = list("Percent White" = 1, "Percent Black" = 2,
-                                 "Percent Hispanic" = 3,  "Percent Asian" = 4), selected = 1),
+      helpText("Create demographic maps with 
+        information from the 2010 US Census."),
       
-      sliderInput("slider1", label = h3("Range of interest"),
-                  min = 0, max = 100, value = 0),
-
-      img(src="bigorb.png", height = 72, width = 72),
-      "shiny is a product of ", 
-      span("RStudio", style = "color:blue")
+      selectInput("var", 
+                  label = "Choose a variable to display",
+                  choices = c("Percent White", "Percent Black",
+                              "Percent Hispanic", "Percent Asian"),
+                  selected = "Percent White"),
+      
+      sliderInput("range", 
+                  label = "Range of interest:",
+                  min = 0, max = 100, value = c(0, 100))
     ),
-  
-      mainPanel(
-      h1("Introducting Shiny"),
-      p("Shiny is a new package from RStudio that makes it incredibly easy to build interactive web application with R"),
-      br(),
-      p("For an introduction and live examples, visit the Shiny homepage",
-        a("Shiny homepage.", 
-          href = "http://www.rstudio.com/shiny")
-        ),
-      br(),
-      h1("Features"),
-      p("* Build useful web applications with only a few lines of code - no javascript required"),
-      p("* Shiny applications are autometically \"live\" in the same way that",
-        strong("spreadsheets"),
-        "are live. Outputs change instantly as users modify inputs, without requiring a reload of the browser")
-      
+    
+    mainPanel(
+      h1(textOutput("text1")),
+      h2(textOutput("text2"))
     )
   )
 ))
